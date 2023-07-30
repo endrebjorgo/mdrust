804db9d3f3d43d07f886109f1267289c23e696ec
endrebjorgo <endrebjorgo@gmail.com>
2023-07-30 22:46:42 +0200

    Add TypedLine struct

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,113 @@
 use std::env;
 use std::fs;
-use std::str::Chars;
+use std::process::exit;
 
 
-fn lex_md(md_file: String) {
-    for line in md_file.lines() {
-        lex_md_line(line);
+fn tokenize(line: &str) -> Vec<&str> {
+    let line_vec: Vec<char> = line.chars().collect();
+    let mut start: usize = 0;
+    let mut curr: usize = 0;
+    let mut result: Vec<&str> = Vec::new();
+
+    while curr < line.len() {
+        if !line_vec[curr].is_whitespace() {
+            curr += 1;
+        } else {
+            result.push(&line[start..curr]);
+            curr += 1;
+            start = curr;
+        }
+    }
+    if start != curr {
+        result.push(&line[start..]);
+    }
+    result
+}
+
+#[derive(PartialEq)]
+enum LineType {
+    H1,
+    H2,
+    H3,
+    H4,
+    H5,
+    H6,
+    Ul,
+    Paragraph,
+}
+
+struct TypedLine {
+    line_type: LineType,
+    value: String,
+}
+
+fn parse_tokens_to_type(tokens: Vec<&str>) -> TypedLine {
+    if tokens.len() == 0 {
+        return TypedLine {
+            line_type: LineType::Paragraph,
+            value: "".to_string(),
+        }
+    }
+
+    let mut lt: LineType;
+
+    let first_token: &str = tokens[0];
+
+    match first_token {
+        "#" => lt = LineType::H1,
+        "##" => lt = LineType::H2,
+        "###" => lt = LineType::H3,
+        "####" => lt = LineType::H4,
+        "#####" => lt = LineType::H5,
+        "######" => lt = LineType::H6,
+        "-" => lt = LineType::Ul,
+        _ => lt = LineType::Paragraph,
+    }
+    
+    let lv: String;
+    
+    if lt == LineType::Paragraph {
+        lv = tokens.join(" ");
+    } else {
+        lv = tokens[1..].join(" ");
+    }
+
+    TypedLine{
+        line_type: lt, 
+        value: lv,
     }
 }
 
-fn lex_md_line(md_line: &str) {
-    // println!("{}", md_line.to_string());
-    let mut md_line_chars: Chars = md_line.chars();
-    println!("{}", md_line_chars.nth(0).unwrap_or(' '));
+fn markdown_to_html(file_path: &String) -> String {
+    let contents: String = fs::read_to_string(file_path).expect("ERROR: could not read file");
+    let lines: Vec<&str> = contents.lines().collect();
+    let mut html_contents: String = String::new();
+
+    for line in lines {
+        let tokens: Vec<&str> = tokenize(line);    
+        let tl: TypedLine = parse_tokens_to_type(tokens);
+        html_contents.push_str(&tl.value);
+        html_contents.push_str("\n");
+    }
+    html_contents
 }
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let args_len: usize = args.len();
+    let argv: Vec<String> = env::args().collect();
+    let argc: usize = argv.len() - 1;
 
     // TODO: assert correct filetype
     // TODO: flag to indicate direction of translation
 
-    if args_len != 2 {
-        eprintln!("ERROR: expected 1 argument, but got {}", args_len-1);
-        panic!();
+    if argc != 1 {
+        eprintln!("ERROR: expected 1 argument, but got {argc}");
+        exit(1);
         // TODO: handle this error properly  
     }
 
-    let file_path: &String = &args[1];
+    let file_path: &String = &argv[1];
 
-    let contents: String = fs::read_to_string(file_path)
-        .expect("ERROR: could not read file");
-
-    lex_md(contents);
+    let html = markdown_to_html(&file_path);
+    println!("{html}");
 }
 
