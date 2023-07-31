1fc4e2902a218363e8297a0997ede4ea85be6ab4
endrebjorgo <endrebjorgo@gmail.com>
2023-07-31 12:31:49 +0200

    Rework tokenize()

--- src/main.rs
+++ src/main.rs
@@ -18,65 +18,117 @@ fn md_to_html(md_file: &String) -> String {
 }
 
 fn md_to_html_line(line: &str) -> String {
-    let tokens: Vec<&str> = tokenize(line);    
-    let typed_line = tokens_to_typed_line(tokens);
-    format!("{} {}", typed_line.line_type.to_string(), typed_line.value)
+    let ltv = line_token_value(line);
+    format!("First token: '{}'\nValue: '{}'\n", ltv.0, ltv.1)
 }
 
-fn tokenize(line: &str) -> Vec<&str> {
-    let line_vec: Vec<char> = line.chars().collect();
-    let mut start: usize = 0;
-    let mut curr: usize = 0;
-    let mut result: Vec<&str> = Vec::new();
+fn line_token_value(line: &str) -> (&str, &str) {
+    let chars = line.chars().collect::<Vec<char>>();
+    let mut ft_start: usize = 0;
+    let mut ft_end: usize; 
 
-    while curr < line.len() {
-        if !line_vec[curr].is_whitespace() {
-            curr += 1;
-        } else {
-            result.push(&line[start..curr]);
-            curr += 1;
-            start = curr;
-        }
+    while ft_start < chars.len() && chars[ft_start].is_whitespace() {
+        ft_start += 1;
+    } 
+    if ft_start == chars.len() {
+        // The whole line was a whitespace
+        return ("", "");
     }
-    if start != curr {
-        result.push(&line[start..]);
+    ft_end = ft_start + 1; 
+
+    while ft_end < chars.len() && !chars[ft_end].is_whitespace() {
+        ft_end += 1;
     }
-    result
+    let first_token: &str = &line[ft_start..ft_end];
+    let value: &str = &line[ft_end..];
+    (first_token, value)
 }
 
-fn tokens_to_typed_line(tokens: Vec<&str>) -> TypedLine {
-    if tokens.len() == 0 {
-        return TypedLine {
-            line_type: LineType::EmptyLine,
-            value: "".to_string(),
-        }
-    }
-    let first_token: &str = tokens[0];
-    let lt: LineType;
-
-    match first_token {
-        "#" => lt = LineType::H1,
-        "##" => lt = LineType::H2,
-        "###" => lt = LineType::H3,
-        "####" => lt = LineType::H4,
-        "#####" => lt = LineType::H5,
-        "######" => lt = LineType::H6,
-        "-" => lt = LineType::Ul,
-        _ => lt = LineType::Paragraph,
-    }
-    let lv: String;
-    
-    if lt == LineType::Paragraph {
-        lv = tokens.join(" ");
-    } else {
-        lv = tokens[1..].join(" ");
-    }
-
-    TypedLine{
-        line_type: lt, 
-        value: lv,
-    }
-}
+// fn tokenize(line: &str) -> (&str, &str){
+//     let chars = line.chars().collect::<Vec<char>>;
+//     let start_token: &str;
+//     let value: &str;
+// 
+//     let mut cursor: usize = 0;
+//     let mut pivot: usize;
+//     let mut curr_char: char;
+// 
+//     while cursor < chars.len() {
+//         curr_char = chars[cursor];
+//         if curr_char.is_whitespace() {
+//             cursor += 1;
+//         } else if curr_char == '>' {
+//             pivot = cursor + 1;
+//         } else if curr_char == '#' {
+//             pivot= cursor +1;
+//             while chars[pivot] == '#' {
+//                 pivot+= 1;
+//             }
+//         } else if curr_char == '>' {
+//             
+//         }
+//     }
+//     start_token = chars[cursor..pivot].collect();
+//     value.push_str(chars[end..].collect());
+//     (start_token, value)
+// }
+// 
+// fn tokenize(line: &str) -> Vec<&str> {
+//     let line_vec: Vec<char> = line.chars().collect();
+//     let mut start: usize = 0;
+//     let mut curr: usize = 0;
+//     let mut result: Vec<&str> = Vec::new();
+// 
+//     while curr < line.len() {
+//         if !line_vec[curr].is_whitespace() {
+//             curr += 1;
+//         } else {
+//             result.push(&line[start..curr]);
+//             curr += 1;
+//             start = curr;
+//         }
+//     }
+//     if start != curr {
+//         result.push(&line[start..]);
+//     }
+//     result
+// }
+// 
+// fn tokens_to_typed_line(tokens: Vec<&str>) -> TypedLine {
+//     if tokens.len() == 0 {
+//         return TypedLine {
+//             line_type: LineType::EmptyLine,
+//             value: "".to_string(),
+//         }
+//     }
+//     let lt: LineType;
+//     let first_token: &str = tokens[0];
+//     let first_char = first_token.chars.nth(0).unwrap();
+// 
+//     match first_token {
+//         "#" => lt = LineType::H1,
+//         "##" => lt = LineType::H2,
+//         "###" => lt = LineType::H3,
+//         "####" => lt = LineType::H4,
+//         "#####" => lt = LineType::H5,
+//         "######" => lt = LineType::H6,
+//         "-" => lt = LineType::Ul,
+//         _ => lt = LineType::Paragraph,
+//     }
+// 
+//     let lv: String;
+//     
+//     if lt == LineType::Paragraph {
+//         lv = tokens.join(" ");
+//     } else {
+//         lv = tokens[1..].join(" ");
+//     }
+// 
+//     TypedLine{
+//         line_type: lt, 
+//         value: lv,
+//     }
+// }
 
 #[derive(PartialEq)]
 enum LineType {
@@ -89,6 +141,7 @@ enum LineType {
     Ul,
     Paragraph,
     EmptyLine,
+    Blockquote,
 }
 
 impl ToString for LineType {
@@ -103,6 +156,7 @@ impl ToString for LineType {
             LineType::Ul => "Ul".to_string(),
             LineType::Paragraph => "Paragraph".to_string(),
             LineType::EmptyLine => "EmptyLine".to_string(),
+            LineType::Blockquote => "Blockquote".to_string(),
         }
     }
 }
@@ -130,4 +184,5 @@ fn main() {
     let html = md_to_html(&file_path);
     println!("{html}");
 }
+
 
